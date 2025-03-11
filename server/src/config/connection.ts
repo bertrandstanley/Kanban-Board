cb2f800d3d81ad7f57f3b6fd74b35f10ccf169b5
bertrandstanley <bertrandstanley@gmail.com>
2025-03-11 13:19:16 -0400

    Update .env and Sequelize connection configuration

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -3,19 +3,18 @@ dotenv.config();
 
 import { Sequelize } from 'sequelize';
 
-const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(
-      process.env.DB_NAME || '',
-      process.env.DB_USER || '',
-      process.env.DB_PASSWORD,
-      {
-        host: 'localhost',
-        dialect: 'postgres',
-        dialectOptions: {
-          decimalNumbers: true,
-        },
-      }
-    );
+const sequelize = new Sequelize(
+  process.env.DB_NAME || '',
+  process.env.DB_USER || '',
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST || 'localhost', // Use DB_HOST from env, or default to 'localhost'
+    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 5432, // Use DB_PORT from env, or default to 5432
+    dialect: 'postgres',
+    dialectOptions: {
+      decimalNumbers: true,
+    },
+  }
+);
 
 export default sequelize;
