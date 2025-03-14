45b86aaf24cb00f00c6fcdfc4aba466b96ae9bdd
bertrandstanley <bertrandstanley@gmail.com>
2025-03-14 03:48:44 -0400

    Update with users.sql to create the users in render database

--- users.sql
+++ users.sql
@@ -1,5 +1,8 @@
--- Ensure that the table exists before inserting (adjust as necessary)
-CREATE TABLE IF NOT EXISTS "users" (
+-- Drop the users table if it exists
+DROP TABLE IF EXISTS "users";
+
+-- Create the users table
+CREATE TABLE "users" (
   "id" SERIAL PRIMARY KEY,
   "username" VARCHAR(255) UNIQUE NOT NULL,
   "password" VARCHAR(255) NOT NULL,
